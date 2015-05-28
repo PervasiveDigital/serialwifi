49c63ef538aec082b5a514daab09dbced1237908
Martin Calsyn <martin@pervasive.digital>
2015-05-28 13:33:52 +0300

    Update

--- src/tests/AzureBlobTest/Program.cs
+++ src/tests/AzureBlobTest/Program.cs
@@ -15,11 +15,12 @@ namespace AzureBlobTest
         {
             // For neon
             var wifi = new Esp8266WifiDevice(new SerialPort("COM2", 115200, Parity.None, 8, StopBits.One), new OutputPort((Cpu.Pin)19, false), null);
-            wifi.Connect("CloudGate", "Escal8shun");
+            wifi.Connect("Xxxxx", "Xxxxx");
 
             var sntp = new SntpClient(wifi, "time1.google.com");
             sntp.SetTime();
 
+            // You have to change this - the storage account does not exist and the key is not valid
             var account = new CloudStorageAccount("pdazurestoragedemo", "PblOktyRKcKQy+5qMe3doPcxzcUqKU38ZBTGCOs8g+10CamdApbJd1FDw2WTsZ8/k+LoUvCQsc4NPBZ0jY/9DA==");
             var blob = new BlobClient(wifi, account);
 
